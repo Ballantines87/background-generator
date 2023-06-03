3200d9afe97444ac2ef47468bd9169a1c0b654a1
Paul M <66521395+Ballantines87@users.noreply.github.com>
2023-06-03 03:12:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-var css = document.querySelector("h3");
+var h3 = document.querySelector("h3");
 var color1 = document.getElementById("1");
 var color2 = document.getElementById("2");
-
-
 
 color1.addEventListener("input", (e) => {
     var body = document.querySelector("body");
     body.style.background = `linear-gradient(to right, ${e.target.value}, ${color2.value})`;
+    showChanges(body);
 })
 
 color2.addEventListener("input", (e) => {
     var body = document.querySelector("body");
     body.style.background = `linear-gradient(to right, ${color1.value}, ${e.target.value})`;
+    showChanges(body);
 })
+
+function showChanges(body) {
+    h3.textContent = body.style.background + ";";
+}
