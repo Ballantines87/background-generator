51ade98324261f8d1eb92b16129cce9a0cf729ac
Paul M <66521395+Ballantines87@users.noreply.github.com>
2023-06-03 02:53:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,14 +5,11 @@ var color2 = document.getElementById("2");
 
 
 color1.addEventListener("input", (e) => {
-    console.log(color1);
-    console.log(e.target.value);
     var body = document.querySelector("body");
     body.style.background = `linear-gradient(to right, ${e.target.value}, ${color2.value})`;
 })
 
 color2.addEventListener("input", (e) => {
-    console.log(e.target.value);
     var body = document.querySelector("body");
     body.style.background = `linear-gradient(to right, ${color1.value}, ${e.target.value})`;
 })
